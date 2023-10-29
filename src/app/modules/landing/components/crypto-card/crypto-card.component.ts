d20f33f860e028bd5c5ca83674d43e867ce36b22
ianpatrani <iannpatrnai@gmail.com>
2023-10-29 13:04:21 -0300

    refactor : cryptos cards

--- src/app/modules/landing/components/crypto-card/crypto-card.component.ts
+++ src/app/modules/landing/components/crypto-card/crypto-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crypto-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crypto-card.component.css']
 })
 export class CryptoCardComponent implements OnInit {
+
+  @Input() titulo: string | undefined;
+  @Input() precio: string | undefined;
+  @Input() imageUrl: string | undefined;
+  @Input() porcentaje: string | undefined;
 
   constructor() { }
 
