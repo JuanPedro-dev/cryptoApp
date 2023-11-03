1522dfbd80d4fb668a83e2f6b719387bf1f7dfe2
Juan Manuel Da Silva <juanmanuel_dasilva@hotmail.com>
2023-11-03 01:37:51 -0300

    creacion de componente para calcular valores de criptos en distintas divisas

---
+++ src/app/core/Models.ts
@@ -0,0 +1,17 @@
+// TODO: Borrar si no se usa
+
+import { IUser } from './Interfaces';
+
+export class User implements IUser {
+  id: number | null;
+  userName: string | null;
+  email: string | null;
+  password: string | null;
+
+  constructor(user?: any) {
+    this.id = user.id != null ? user.id : null;
+    this.userName = user.userName != null ? user.userName : null;
+    this.email = user.email != null ? user.email : null;
+    this.password = user.password != null ? user.password : null;
+  }
+}
